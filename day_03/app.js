b85fd3298742e60ad24dca1aa7b8509cc4224b1f
austinChappell <Austin.W.Chappell@gmail.com>
2017-12-04 12:51:45 -0600

    add comments

--- day_03/app.js
+++ day_03/app.js
@@ -1,66 +1,45 @@
 function manhattanDist(num) {
     let squareRoot = Math.ceil(Math.sqrt(num));
     squareRoot = squareRoot % 2 === 1 ? squareRoot : squareRoot + 1;
-    console.log('SQUARE ROOT', squareRoot);
 
+    // Get the max corner
     const cornerNum = squareRoot * squareRoot;
-    console.log('CORNER NUMBER', cornerNum);
+
+    // Get halfway from corner to middle of side
     const halfWay = Math.floor(squareRoot / 2);
-    const edgeDiff = squareRoot - 1;
-    console.log('EDGE DIFF', edgeDiff);
-    console.log('HALFWAY', halfWay);
 
+    // Get the difference from one corner to the next
+    const edgeDiff = halfWay * 2;
+
+    // Just the corners
     const cornerArr = [cornerNum, cornerNum - edgeDiff, cornerNum - edgeDiff * 2, cornerNum - edgeDiff * 3];
-    const mids = [cornerArr[0] - halfWay, cornerArr[1] - halfWay, cornerArr[2] - halfWay, cornerArr[3] - halfWay];
-    console.log('CORNER ARRAY', cornerArr);
-    console.log('MIDS', mids);
 
-    // cornerArr.forEach((item) => {
-    //     const diffToCorner = Math.abs(item - num);
-    //     const diffToHalfway = Math.abs(item - halfWay - num);
-    //     console.log('DIFF TO CORNER', diffToCorner);
-    //     console.log('DIFF TO HALFWAY', diffToHalfway);
-    //     if (diffToCorner < halfWay) {
-    //         console.log('CLOSE TO HALFWAY');
-    //     } else if (diffToHalfway < halfWay) {
-    //         console.log('CLOSE TO CORNER');
-    //     } else if (diffToHalfway === halfWay) {
-    //         console.log('MATCH');
-    //     }
-    // })
+    // Just the middle of each side
+    const mids = [cornerArr[0] - halfWay, cornerArr[1] - halfWay, cornerArr[2] - halfWay, cornerArr[3] - halfWay];
 
     let notInMiddle = true;
-
     let steps = 0;
 
     while (notInMiddle) {
 
-
         for (let i = 0; i < 4; i++) {
             if (num < cornerArr[i] && num > mids[i]) {
-                console.log('FIRST HALF');
                 num--;
                 steps++;
             } else if (num > cornerArr[i] && num < mids[i]) {
-                console.log('SECOND HALF');
                 num++;
                 steps++;
             } else if (num === cornerArr[i]) {
-                console.log('CORNER');
                 num--;
                 steps++;
             } else if (num === mids[i]) {
-                console.log('MIDDLE');
                 notInMiddle = false;
             }
         }
-        console.log(num);
 
     }
 
-    console.log(steps);
-
-    console.log('TOTAL', steps + halfWay);
+    return steps + halfWay;
 
 }
 
